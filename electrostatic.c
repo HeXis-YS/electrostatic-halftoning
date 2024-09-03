b3c5951be929ab68418518fd9fbe2be0b536824e
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2024-09-03 16:46:06 +0800

    Refactor

--- electrostatic.c
+++ electrostatic.c
@@ -1,6 +1,6 @@
 #include "cv.hpp"
-#include <float.h>
 #include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +8,14 @@
 #define possibility(x, y) __builtin_expect_with_probability(x, 1, y)
 #define likely(x) __builtin_expect(x, 1)
 #define unlikely(x) __builtin_expect(x, 0)
+
+#define malloc_double(size) (double *)malloc(sizeof(double) * size)
+#define malloc_uint8(size) (uint8_t *)malloc(sizeof(uint8_t) * size)
+#define zero_double(ptr, size) memset(ptr, 0, sizeof(double) * size)
+
+#define one_or_zero(x) x = x ? 1 : 0;
+
+#define pow2(x) (x * x)
 
 static double rand_double() {
 	return (double)rand() / ((double)RAND_MAX + 1.0);
@@ -24,11 +32,11 @@ int electrostatic_halftoning(const char *src_path,
 							 int enable_debug) {
 	/* Exceptions */
 	max_iterations = (max_iterations > 0) ? max_iterations : 8;
-	enable_initial_charge = enable_initial_charge ? 1 : 0;
-	enable_gridforce = enable_gridforce ? 1 : 0;
-	enable_shake = enable_shake ? 1 : 0;
+	one_or_zero(enable_initial_charge);
+	one_or_zero(enable_gridforce);
+	one_or_zero(enable_shake);
 	enable_early_stop = enable_early_stop ? enable_early_stop : 0;
-	enable_debug = enable_debug ? 1 : 0;
+	one_or_zero(enable_debug);
 	if (color_depth < 1 || color_depth > 7) {
 		printf("Error: Color depth = [1, 7]\n");
 	}
@@ -52,9 +60,9 @@ int electrostatic_halftoning(const char *src_path,
 	const int rows = src.rows;
 	const int cols = src.cols;
 	const int pixel_count = rows * cols;
-	double *image_in = (double *)malloc(sizeof(double) * pixel_count);
-	unsigned char *image_dst = (unsigned char *)malloc(sizeof(unsigned char) * pixel_count);
-	unsigned char *image_level = (unsigned char *)malloc(sizeof(unsigned char) * pixel_count);
+	double *image_in = malloc_double(pixel_count);
+	uint8_t *image_dst = malloc_uint8(pixel_count);
+	uint8_t *image_level = malloc_uint8(pixel_count);
 	struct CMat dst;
 	dst.rows = rows;
 	dst.cols = cols;
@@ -63,8 +71,8 @@ int electrostatic_halftoning(const char *src_path,
 	/* Color Depth */
 	const int pixel_level_max = (1 << color_depth) - 1;
 	const double particle_charge = 1.0 / (double)pixel_level_max;
-	const double particle_charge_2 = 1.0 / (double)(pixel_level_max * pixel_level_max);
-	unsigned char pixel_level[128];
+	const double particle_charge_2 = 1.0 / (double)pow2(pixel_level_max);
+	uint8_t pixel_level[128];
 	for (int i = 0; i <= pixel_level_max; i++) {
 		pixel_level[i] = (255 * i + pixel_level_max / 2) / pixel_level_max;
 	}
@@ -80,9 +88,9 @@ int electrostatic_halftoning(const char *src_path,
 	printf("Particle count = %d\n", particle_count);
 
 	/* Particle Initialization */
-	double *particle_Y = (double *)malloc(sizeof(double) * particle_count);
-	double *particle_X = (double *)malloc(sizeof(double) * particle_count);
-	memset(image_level, pixel_level_max, sizeof(unsigned char) * pixel_count);
+	double *particle_Y = malloc_double(particle_count);
+	double *particle_X = malloc_double(particle_count);
+	memset(image_level, pixel_level_max, sizeof(uint8_t) * pixel_count);
 	for (int particle = 0; particle < particle_count;) {
 		int rand_Y = rand() % rows;
 		int rand_X = rand() % cols;
@@ -104,12 +112,12 @@ int electrostatic_halftoning(const char *src_path,
 	}
 
 	/* Process */
-	double *particle_Y_last = (double *)malloc(sizeof(double) * particle_count);
-	double *particle_X_last = (double *)malloc(sizeof(double) * particle_count);
-	double *distance_X = (double *)malloc(sizeof(double) * cols);
-	double *distance_X_2 = (double *)malloc(sizeof(double) * cols);
-	double *force_Y_array = (double *)malloc(sizeof(double) * particle_count);
-	double *force_X_array = (double *)malloc(sizeof(double) * particle_count);
+	double *particle_Y_last = malloc_double(particle_count);
+	double *particle_X_last = malloc_double(particle_count);
+	double *distance_X = malloc_double(cols);
+	double *distance_X_2 = malloc_double(cols);
+	double *force_Y_array = malloc_double(particle_count);
+	double *force_X_array = malloc_double(particle_count);
 	// Shake
 	int shake = 0;
 	double shake_tmp;
@@ -120,22 +128,22 @@ int electrostatic_halftoning(const char *src_path,
 	// Time step
 	double time_step = 0.1;
 	double mean;
-	// Early stop
-	unsigned char *image_last = NULL;
-	int early_stop_counter = 0;
-	if (enable_early_stop) {
-		image_last = (unsigned char *)malloc(sizeof(unsigned char) * pixel_count);
-	}
 	if (enable_debug) {
 		printf("Time step = %f\n", time_step);
+	}
+	// Early stop
+	uint8_t *image_last = NULL;
+	int early_stop_counter = 0;
+	if (enable_early_stop) {
+		image_last = malloc_uint8(pixel_count);
 	}
 	printf("\n");
 	for (int current_iteration = 1; current_iteration <= max_iterations; current_iteration++) {
 		printf("Iteration %d\n", current_iteration);
 		memcpy(particle_Y_last, particle_Y, sizeof(double) * particle_count);
 		memcpy(particle_X_last, particle_X, sizeof(double) * particle_count);
-		memset(force_Y_array, 0, sizeof(double) * particle_count);
-		memset(force_X_array, 0, sizeof(double) * particle_count);
+		zero_double(force_Y_array, particle_count);
+		zero_double(force_X_array, particle_count);
 		if (enable_shake) {
 			if (possibility(current_iteration % 10 == 0, 0.1)) {
 				shake_tmp1 = shake_tmp * exp(current_iteration / 1000.0);
@@ -145,9 +153,9 @@ int electrostatic_halftoning(const char *src_path,
 			}
 		}
 		if (enable_early_stop) {
-			memcpy(image_last, image_level, sizeof(unsigned char) * pixel_count);
+			memcpy(image_last, image_level, sizeof(uint8_t) * pixel_count);
 		}
-		memset(image_level, pixel_level_max, sizeof(unsigned char) * pixel_count);
+		memset(image_level, pixel_level_max, sizeof(uint8_t) * pixel_count);
 		for (int current_particle = 0; current_particle < particle_count; current_particle++) {
 			double force_Y = 0.0;
 			double force_X = 0.0;
@@ -160,11 +168,11 @@ int electrostatic_halftoning(const char *src_path,
 			tmp = 0.5 - particle_X_current;
 			for (int x = 0; x < cols; x++) {
 				distance_X[x] = tmp;
-				distance_X_2[x] = tmp * tmp;
+				distance_X_2[x] = pow2(tmp);
 				tmp += 1.0;
 			}
 			for (int y = 0, p = 0; y < rows; y++) {
-				double distance_Y_2 = distance_Y * distance_Y;
+				double distance_Y_2 = pow2(distance_Y);
 				for (int x = 0; x < cols; x++, p++) {
 					if (unlikely(image_in[p] == 0.0)) {
 						continue;
@@ -183,7 +191,7 @@ int electrostatic_halftoning(const char *src_path,
 			for (int particle = current_particle + 1; particle < particle_count; particle++) {
 				double distance_Y = particle_Y_last[particle] - particle_Y_current;
 				double distance_X = particle_X_last[particle] - particle_X_current;
-				tmp = distance_Y * distance_Y + distance_X * distance_X;
+				tmp = pow2(distance_Y) + pow2(distance_X);
 				tmp = (tmp <= 0.1) ? (20.0 - 100.0 * tmp) : (1.0 / tmp);
 				tmp *= particle_charge_2;
 				double repulsion_force = distance_Y * tmp;
@@ -200,7 +208,7 @@ int electrostatic_halftoning(const char *src_path,
 				double grid_distance_X = particle_X_current - (int)particle_X_current;
 				grid_distance_Y = possibility(grid_distance_Y <= 0.5, 0.5) ? -grid_distance_Y : 1 - grid_distance_Y;
 				grid_distance_X = possibility(grid_distance_X <= 0.5, 0.5) ? -grid_distance_X : 1 - grid_distance_X;
-				tmp = grid_distance_Y * grid_distance_Y + grid_distance_X * grid_distance_X;
+				tmp = pow2(grid_distance_Y) + pow2(grid_distance_X);
 				if (likely(tmp != 0.0)) {
 					tmp = sqrt(tmp);
 					tmp = 3.5 / (tmp + 10000.0 * pow(tmp, 9.0));
@@ -238,7 +246,7 @@ int electrostatic_halftoning(const char *src_path,
 			for (int current_particle = 0; current_particle < particle_count; current_particle++) {
 				double force_Y = force_Y_array[current_particle];
 				double force_X = force_X_array[current_particle];
-				mean += sqrt(force_Y * force_Y + force_X * force_X);
+				mean += sqrt(pow2(force_Y) + pow2(force_X));
 			}
 			mean /= (double)particle_count;
 			printf("Mean force = %f\n", mean);
@@ -253,7 +261,7 @@ int electrostatic_halftoning(const char *src_path,
 			cv_imwrite(out_file, dst);
 		}
 		if (enable_early_stop) {
-			if (memcmp(image_level, image_last, sizeof(unsigned char) * pixel_count) == 0) {
+			if (memcmp(image_level, image_last, sizeof(uint8_t) * pixel_count) == 0) {
 				early_stop_counter++;
 				printf("Result unchanged for %d iterations.\n", early_stop_counter);
 				if (early_stop_counter >= enable_early_stop) {
